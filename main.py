f65dd60b252468997e4ff9ebca88df4865ee09c1
radin-111 <radinsheikh87@gmail.com>
2025-04-14 21:42:56 +0600

    Completing all tasks

--- main.py
+++ main.py
@@ -39,3 +39,46 @@ if a < b and b < c:
 
 if a == b or a < b :
     print("A is equal or smaller than B")
+if not(a > b and b > c ):
+    print("B is greater than A and C")
+
+# Task 5
+
+num = 6
+num +=5
+print(num)
+num -= 3
+print(num)
+num *= 8
+print(num)
+num /= 16
+print(num)
+
+# Task 6
+
+num = 9
+n = 0
+
+print(n is num)
+
+print(n is not num)
+
+numbers = [1,3,556,2]
+numbers2 = [2,4,5,67]
+cnum = numbers
+print(numbers is numbers2)
+print(numbers is not numbers2)
+print(numbers is cnum)
+print(numbers is not cnum)
+
+# Task 7
+cars = ['BMW','Toyota','Ferrari']
+
+if "BMW" in cars:
+    print('BMW is available')
+
+if "Bugati" not in cars:
+    print("Bugati is not here")
+
+if "Ferrari" not in cars:
+    print('Ferrari is not here')
