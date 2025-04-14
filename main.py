07eebf32beb44ad6d0d5affd3fc4a9d1e9d3a1fa
radin-111 <radinsheikh87@gmail.com>
2025-04-14 21:30:14 +0600

    completing task 3,4

--- main.py
+++ main.py
@@ -18,3 +18,24 @@ print(add)
 print(sub)
 print(multiply)
 print(division)
+
+# Task 3
+
+if age == 18:
+    print("you are 18 years old")
+
+if age > 18:
+    print("Your age is greater than 18")
+if age != 18:
+    print("you are not 18 years old")
+
+# Task 4
+a = 5
+b = 10
+c = 15
+
+if a < b and b < c:
+    print("B is greater than A but C is greater than B ")
+
+if a == b or a < b :
+    print("A is equal or smaller than B")
